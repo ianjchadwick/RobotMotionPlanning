db193d412748f14dba4fd5315f95936c517452df
Ian Chadwick <ianjchadwick@gmail.com>
2021-11-26 21:23:34 -0500

    Answer to Q2.2

--- HW4/graph_search_startGoal.m
+++ HW4/graph_search_startGoal.m
@@ -6,3 +6,13 @@
 %xPath from  @x   idxStart to  @x   idxGoal.   @x   xStart and  @x   xGoal,
 %respectively, to the beginning and the end of the array  @x   xPath. enumerate
 function [xPath]=graph_search_startGoal(graphVector,xStart,xGoal)
+
+% Get node closest to start and goal
+idxStart = graph_nearestNeighbors(graphVector, xStart,1);
+idxGoal = graph_nearestNeighbors(graphVector, xGoal,1);
+
+% Use graph search to find path
+[xPath, graphVector] = graph_search(graphVector,idxStart,idxGoal);
+
+% Append xStart to begining and xGoal to end of xPath
+xPath = [xStart, xPath, xGoal];
