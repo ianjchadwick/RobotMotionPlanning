ac987f80b221addc520807e58ff07f97589a01d0
Ian Chadwick <ianjchadwick@gmail.com>
2021-11-26 20:17:13 -0500

    Revert "Q1.5 completed"
    This reverts commit 6e1f958dc3d2bd84f0c9d88aae76ddfa76dbffae.

--- HW4/graph_search.m
+++ HW4/graph_search.m
@@ -3,36 +3,6 @@
 %alg:astar.
 function [xPath,graphVector]=graph_search(graphVector,idxStart,idxGoal)
 
-% Initialize priority queue, closed node list, and values for start node
-pqOpen = priority_prepare();
-idxClosed = []
-graphVector(idxStart).backpointer = [];
-graphVector(idxStart).g = 0;
-%Add start node to the queue
-pqOpen = priority_insert(pqOpen,idxStart,0);
-idxExpand = [];
-
-while ~isempty(pqOpen)
-    [pqOpen, idxNBest, ~] = priority_minExtract(pqOpen);
-    idxClosed(size(idxClosed,2)+1) = idxNBest;
-    
-    if idxNBest == idxGoal
-        break;
-    end
-    
-    idxExpand = graph_getExpandList(graphVector, idxNBest, idxClosed);
-    sizeExpand = size(idxExpand,1);
-    
-    for idx=1:sizeExpand
-        [graphVector, pqOpen] = graph_expandElement(graphVector, idxNBest,idxExpand(idx,1), idxGoal, pqOpen);     
-    end
-    
-end
-
-xPath = graph_path(graphVector, idxStart, idxGoal);
-
-
-
 % Set a maximum limit of iterations in the main  @x   A* loop on line 
 %it:astar-main-loop of Algorithm  alg:astar. This will prevent the algorithm from
 %remaining stuck on malformed graphs (e.g., graphs containing a node as a
