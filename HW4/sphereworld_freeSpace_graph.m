ce3900e66741dcaac38c35db78d5bc1aadd24846
Ian Chadwick <ianjchadwick@gmail.com>
2021-11-26 20:36:27 -0500

    Update sphereworld_freeSpace_graph.m

--- HW4/sphereworld_freeSpace_graph.m
+++ HW4/sphereworld_freeSpace_graph.m
@@ -11,3 +11,21 @@
 %the grid); note that the choice of the attractive potential here does not
 %matter.  grid2graph.  the resulting  @x   graphVector structure. enumerate
 function [graphVector]=sphereworld_freeSpace_graph(NCells)
+
+load("sphereworld.mat");
+
+grid = grid_init(NCells);
+potential = struct('xGoal', xGoal(:,2), 'shape', 'conic', 'repulsiveWeight', 0.01);
+
+potHandle = @(xEval) potential_total(xEval, world, potential);
+
+grid = grid_eval(grid,potHandle);
+
+for idxRow=1:NCells
+    for idxCol=1:NCells
+        grid.F(idxRow,idxCol) = ~isnan(grid.F(idxRow,idxCol));
+    end
+end
+
+graphVector = grid2graph(grid);
+
