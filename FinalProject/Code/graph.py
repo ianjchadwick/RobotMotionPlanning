4305e90be23f136c7ea2e0eff2fef3361553eae9
Ian Chadwick <ianjchadwick@gmail.com>
2021-12-05 23:59:35 -0500

    Update graph.py

--- FinalProject/Code/graph.py
+++ FinalProject/Code/graph.py
@@ -21,9 +21,11 @@ class Node:
 
 
 """
-Create a 'size' x 'size' square ndarray with ones to represent free space and zeros for the spaces blocked by obstacles. 
+Create a 'size' x 'size' square ndarray with ones to represent free space and zeros for the spaces blocked by obstacles.
+The x-axis is the vertical axis and the y-axis is the horizontal axis.
 Obstacles are represented as a list of tuples where each 'obstacle' tuple is composed of the top left starting 
-coodinate and the length in the x and y directions [ [x,y], xLength, yLength] 
+coodinate and the length in the x and y directions [ [x,y], xLength, yLength]
+Output is a an array representation of a grid where 1s represent free space and 0s are spaces blocked by obstacles
 """
 def grid_construct(size, obstacles):
     grid = np.ones([size, size], dtype=int)
@@ -40,11 +42,34 @@ def grid_construct(size, obstacles):
 
     return grid
 
+
+"""Uses a grid where 1s are free space and 0s are blocked spaces to create a list of nodes for each free space where 
+the node_id-1 is the list index for that node and to replace the 1s in the grid with the number of the node_id that 
+represents that space.
+Output: the modified grid and a list of nodes representing the free space"""
+def node_construct(grid):
+    size = len(grid)
+    nodeNum = 1
+
+    nodeList = []
+
+    for x in range(0,size):
+        for y in range(0,size):
+            if grid[x][y] == 1:
+                grid[x][y] = nodeNum
+                node = Node(nodeNum, [x,y])
+                nodeList.append(node)
+                nodeNum = nodeNum +1
+    return [grid, nodeList]
+
+
 # For testing purposes
 if __name__ == "__main__":
 
     obstacles = [[[1, 1], 1, 2], [[1,1], 2, 1]]
 
     grid = grid_construct(4, obstacles)
-
     print(grid)
+    [grid2, nodeList] = node_construct(grid)
+    print(grid2)
+    print(nodeList[2].node_id)
