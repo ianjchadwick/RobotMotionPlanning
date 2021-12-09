d4274b3018a7776f890ebc98b45a57c3eba77a1c
Ian Chadwick <ianjchadwick@gmail.com>
2021-12-09 00:24:01 -0500

    added:distance_manhattan: calculates manhattan distance, node_set_d_exit: the distance to closest exit for each node and node_getNeighbor: sets the neighbor/edge list for each node

--- FinalProject/Code/graph.py
+++ FinalProject/Code/graph.py
@@ -1,31 +1,38 @@
+import sys
 import numpy as np
-import math
+
 
 
 """ 
 Create a node object with fields: 
 node_id = integer
 coords = list with x-y coordinates [x,y]
-d_exit = manhattan distance to nearest exit. initialized at -1
+d_exit = manhattan distance to nearest exit. initialized at maxsize
 safety = distance from a shooter location
 neighbors = list of neighbors by node_id
+cost = g(x) movement cost from "moves so far"
 """
 class Node:
 
     def __init__(self, node_id = 0, coords = [0,0]):
         self.node_id = node_id
         self.coords = coords
-        self.d_exit = -1
+        self.d_exit = sys.maxsize
         self.safety = 0
         self.neighbors = []
+        self.cost = 0
+        self.backpointer = int()
 
 
 """
+Inputs: size of grid, tuple describing obstacle location and dimension
+Output: grid with 1s representing free space and 0 for blocked spaces
+
 Create a 'size' x 'size' square ndarray with ones to represent free space and zeros for the spaces blocked by obstacles.
 The x-axis is the vertical axis and the y-axis is the horizontal axis.
 Obstacles are represented as a list of tuples where each 'obstacle' tuple is composed of the top left starting 
-coodinate and the length in the x and y directions [ [x,y], xLength, yLength]
-Output is a an array representation of a grid where 1s represent free space and 0s are spaces blocked by obstacles
+coodinate and the length in the x and y directions [ [x,y], xLength, yLength]. The Output is a an array representation 
+of a grid where 1s represent free space and 0s are spaces blocked by obstacles
 """
 def grid_construct(size, obstacles):
     grid = np.ones([size, size], dtype=int)
@@ -39,15 +46,68 @@ def grid_construct(size, obstacles):
         for x in range(xStart, xStart+xLength):
             for y in range(yStart, yStart+yLength):
                 grid[x][y] = 0
-
     return grid
 
 
-"""Uses a grid where 1s are free space and 0s are blocked spaces to create a list of nodes for each free space where 
+# Determines that manhattan distance between two points on a grid
+def distance_manhattan(point1, point2):
+    distance = abs(point1[0]-point2[0]) + abs(point1[1]-point2[1])
+    return distance
+
+
+"""
+Inputs: nodeList -> a list of node objects, exitList -> a list of [x,y] coordinates of exits
+Output: updated d_exit values for nodes in nodeList
+
+Find the distance to the closest exit (d_exit) from a list of [x,y] coordinate pairs corresponding to exits in exitList
+and update the node's d_exit with that value
+"""
+def node_set_d_exit(nodeList, exitList):
+    for node in nodeList:
+        for element in exitList:
+            d_exit = distance_manhattan(node.coords, element)
+            if d_exit < node.d_exit:
+                node.d_exit = d_exit
+
+"""
+Inputs: grid -> with node numbers, nodeList -> list of node objects
+Output: updates neighbors attribute for each node in nodeList representing the edge list for that node
+
+Uses updated grid that has node numbers corresponding to their location in each open cell and nodeList to populate each 
+node's neighbors attribute with a list of neighbors by node_id starting CW -> N, E, S, W. These represent the edges 
+between each node
+"""
+def node_getNeighbors(grid,nodelist):
+
+    gridSize = len(grid) - 1
+    for node in nodelist:
+        nodeX = node.coords[0]
+        nodeY = node.coords[1]
+
+        # Add neighbor's node_id from grid if non-zero
+        # check N
+        if nodeX != 0 and grid[nodeX-1][nodeY] !=0:
+            node.neighbors.append(grid[nodeX-1][nodeY])
+        # check E
+        if nodeY != gridSize and grid[nodeX][nodeY+1] !=0:
+            node.neighbors.append(grid[nodeX][nodeY+1])
+        # check S
+        if nodeX != gridSize and grid[nodeX+1][nodeY] !=0:
+            node.neighbors.append(grid[nodeX+1][nodeY])
+        # check W
+        if nodeY != 0 and grid[nodeX][nodeY - 1] != 0:
+            node.neighbors.append(grid[nodeX][nodeY - 1])
+
+
+"""
+Input: Grid with 1s and 0s
+Output: The modified grid and a list of nodes representing the free space
+
+Uses a grid where 1s are free space and 0s are blocked spaces to create a list of nodes for each free space where 
 the node_id-1 is the list index for that node and to replace the 1s in the grid with the number of the node_id that 
 represents that space.
-Output: the modified grid and a list of nodes representing the free space"""
-def node_construct(grid):
+"""
+def node_initialize(grid):
     size = len(grid)
     nodeNum = 1
 
@@ -67,9 +127,19 @@ def node_construct(grid):
 if __name__ == "__main__":
 
     obstacles = [[[1, 1], 1, 2], [[1,1], 2, 1]]
+    exits = [[0, 0], [3, 3]]
 
+
+    testnode = Node(1, [0, 0])
+    testnode.backpointer = 0
     grid = grid_construct(4, obstacles)
     print(grid)
-    [grid2, nodeList] = node_construct(grid)
+    [grid2, nodeList] = node_initialize(grid)
     print(grid2)
-    print(nodeList[2].node_id)
+    node_getNeighbors(grid2, nodeList)
+    node_set_d_exit(nodeList, exits)
+
+    for nodes in nodeList:
+        print(nodes.d_exit)
+
+
